1883a5a6a672b0b5f5a2c7efd006774bf6ec3cad
Marius Gripsgard <mariogrip@ubuntu.com>
2017-11-28 18:48:06 +0100

    Use android headers 24

--- cmake/FindLibHardware.cmake
+++ cmake/FindLibHardware.cmake
@@ -7,7 +7,7 @@
 INCLUDE(FindPackageHandleStandardArgs)
 
 find_package( PkgConfig )
-pkg_search_module(ANDROID_HEADERS REQUIRED android-headers-23)
+pkg_search_module(ANDROID_HEADERS REQUIRED android-headers-24)
 set(LIBHARDWARE_INCLUDE_DIRS ${ANDROID_HEADERS_INCLUDE_DIRS})
 
 find_library(LIBHARDWARE_LIBRARY
